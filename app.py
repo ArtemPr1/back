6d709ab1fbacd7ce9c62115f469ace666c0e6d57
Artem <artemprohorov137@gmail.com>
2024-09-30 22:24:36 +0300

    Реализована версия 1.0 для бэкенда: обработка данных с фронтенда

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask, request
-import json
+from flask import Flask, request, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../front', static_url_path='')
 
+# Обслуживание главной страницы
+@app.route('/')
+def serve_frontend():
+    return send_from_directory(app.static_folder, 'index.html')
+
+# Обслуживание CSS и JS
+@app.route('/css/<path:filename>')
+def serve_css(filename):
+    return send_from_directory(app.static_folder + '/css', filename)
+
+@app.route('/js/<path:filename>')
+def serve_js(filename):
+    return send_from_directory(app.static_folder + '/js', filename)
+
+# Маршрут для приёма данных с фронтенда
 @app.route('/submit', methods=['POST'])
 def submit_data():
     data = request.get_json()
@@ -10,5 +24,6 @@ def submit_data():
         f.write(f"{data['data']}\n")
     return 'Данные успешно сохранены!', 200
 
+# Запуск сервера
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
